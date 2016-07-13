55896ae0271d6df5e243cf3b09e2a6fbf0c8ba0a
shangfeiSF <tony20100125@126.com>
2016-07-13 16:00:48 +0800

    chross 2 项目开发：completeed时注入用户脚本，统计全部iframe的body子节点数

---
+++ public/remote/src/main/injection/web_modules/probe.js
@@ -0,0 +1,76 @@
+function Probe(chross) {
+  var config = config || {}
+
+  this.defaultConfig = {}
+
+  this.config = $.extend(this.defaultConfig, config)
+
+  this.chross = chross
+
+  this.runCodeListeners = {}
+
+  this.init()
+}
+
+$.extend(Probe.prototype, {
+  generateUuid: function () {
+    return [+new Date(), '_', Math.floor(Math.random() * 3e10)].join('')
+  },
+
+  resolveListenerByUuid: function (uuid, data) {
+    var self = this
+
+    if (self.runCodeListeners.hasOwnProperty(uuid)) {
+      self.runCodeListeners[uuid](data)
+    }
+    else {
+      console.error('Can not resolve the runCodeResult')
+    }
+  },
+
+  monitorRunCodeResult: function () {
+    var self = this
+
+    var topPort = self.chross.port.getTopPort()
+
+    topPort.onMessage.addListener(function (msg) {
+      if (msg.uuid === undefined) return false
+      
+      self.resolveListenerByUuid(msg.uuid, msg.data)
+    })
+
+  },
+
+  runCodeInIframe: function (code, config) {
+    var self = this
+
+    var uuid = self.generateUuid()
+
+    var listener = function () {
+    }
+
+    if (config) {
+      if ($.isFunction(config.listener)) {
+        listener = config.listener
+      }
+      else if ($.isFunction(config)) {
+        listener = config
+      }
+    }
+
+    self.runCodeListeners[uuid] = listener
+
+    self.chross.port.post('runCodeInIframe', {
+      uuid: uuid,
+      code: code.toString()
+    })
+  },
+
+  init: function () {
+    var self = this
+
+    self.monitorRunCodeResult()
+  }
+})
+
+module.exports = Probe
