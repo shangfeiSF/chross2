74ed8ff868299c7027909253ad38e8ab57fb588a
shangfeiSF <tony20100125@126.com>
2016-07-22 19:41:25 +0800

    chross 2 项目开发：network get filter API单元测试粒度化

--- server/testPages/remote/main/background/network/units/filterAPIs.js
+++ server/testPages/remote/main/background/network/units/filterAPIs.js
@@ -1,5 +1,5 @@
 $.extend(Suite.prototype, {
-  testFilterAPIs_core: function () {
+  testFilterAPIs: function () {
     var self = this
     var tabId = self.config.mockData.tabId
     var viewStores = self.config.mockData.tabStore.viewStores
@@ -15,6 +15,7 @@ $.extend(Suite.prototype, {
       "onCompleted",
       "onErrorOccurred"
     ]
+    var specialMoment = 'onAllMoments'
 
     var patternURL = 'http://localhost/scripts/1.js'
     var pattern = new RegExp(patternURL)
@@ -39,126 +40,143 @@ $.extend(Suite.prototype, {
       })
     })
 
-    QUnit.test(["filterInAllBVS：在全部的backgroundViewStore中获取全部网络时刻中关于", patternURL, "的请求详情"].join(''), function (assert) {
-      var network = self.create()
+    var specificIndex = 3
 
-      var result = network.filterInAllBVS(network.moments, patternURL, tabId)
+    QUnit.module('filterAPIs', {
+      beforeEach: function () {
+        this.network = self.create()
+      }
+    }, function () {
+      QUnit.module('All', function () {
+        QUnit.module('BVS', function () {
+          function eachAssert(result, assert) {
+            Object.keys(result).forEach(function (moment) {
+              var item = result[moment]
 
-      var msg = ['Passed：', '在全部的backgroundViewStore中获取全部网络时刻中关于', patternURL, '的请求详情'].join('')
-      assert.notStrictEqual(result.data, null, msg)
+              var msg = ['Passed：', '在 All BVS 中 filter 网络时刻', moment, '匹配', patternURL, '的详情'].join('')
+              assert.strictEqual(item.length, 5, msg)
 
-      network.moments.forEach(function (moment) {
-        var msg = ['Passed：', '检查在全部的backgroundViewStore中网络时刻', moment, '中关于', patternURL, '请求详情'].join('')
-        assert.deepEqual(result[moment], expected[moment], msg)
-      })
-    })
-
-    QUnit.test(["filterInCurrentBVS：在当前的backgroundViewStore中获取全部网络时刻中关于", patternURL, "的请求详情"].join(''), function (assert) {
-      var network = self.create()
-
-      var result = network.filterInCurrentBVS(network.moments, patternURL, tabId)
-
-      var msg = ['Passed：', '在当前的backgroundViewStore中获取全部网络时刻中关于', patternURL, '的请求详情'].join('')
-      assert.notStrictEqual(result.data, null, msg)
-
-      network.moments.forEach(function (moment) {
-        var msg = ['Passed：', '检查在当前的backgroundViewStore中网络时刻', moment, '中关于', patternURL, '请求详情'].join('')
-        assert.deepEqual(result[moment], expected[moment][expected[moment].length - 1], msg)
-      })
-    })
-
-    QUnit.test(["filterInSpecificBVS：在指定的backgroundViewStore中获取全部网络时刻中关于", patternURL, "的请求详情"].join(''), function (assert) {
-      var network = self.create()
-
-      var specificIndex = 3
-      var result = network.filterInSpecificBVS(network.moments, patternURL, specificIndex, tabId)
-
-      var msg = ['Passed：', '在指定的backgroundViewStore中获取全部网络时刻中关于', patternURL, '的请求详情'].join('')
-      assert.notStrictEqual(result.data, null, msg)
-
-      network.moments.forEach(function (moment) {
-        var msg = ['Passed：', '检查在指定的backgroundViewStore中网络时刻', moment, '中关于', patternURL, '请求详情'].join('')
-        assert.deepEqual(result[moment], expected[moment][specificIndex], msg)
-      })
-    })
-  },
-
-  testFilterAPIs_derivative: function () {
-    var self = this
-    var tabId = self.config.mockData.tabId
-    var viewStores = self.config.mockData.tabStore.viewStores
-
-    var expected = {}
-    var moments = [
-      "onBeforeRequest",
-      "onBeforeSendHeaders",
-      "onSendHeaders",
-      "onHeadersReceived",
-      "onBeforeRedirect",
-      "onResponseStarted",
-      "onCompleted",
-      "onErrorOccurred"
-    ]
-
-    var patternURL = 'http://localhost/scripts/1.js'
-    var pattern = new RegExp(patternURL)
-
-    moments.forEach(function (moment) {
-      expected[moment] = []
-      viewStores.forEach(function (viewStore) {
-        var data =
-          (moment == 'onBeforeRedirect' || moment == 'onErrorOccurred') ?
-          {
-            key: undefined,
-            value: undefined
-          } :
-          {
-            key: moment,
-            value: viewStore[moment].filter(function (value) {
-              return pattern.exec(value.url) !== null
+              var msg = ['Passed：', '检查在 All BVS 中 filter 网络时刻', moment, '匹配', patternURL, '的详情等于', expected[moment]].join('')
+              assert.deepEqual(item, expected[moment], msg)
             })
           }
 
-        expected[moment].push(data)
+          QUnit.module('Core', function () {
+            QUnit.test(['filterInAllBVS：在 All BVS 中 filter all 网络时刻中匹配', patternURL, '的详情'].join(''), function (assert) {
+              eachAssert(this.network.filterInAllBVS(this.network.moments, patternURL, tabId), assert)
+            })
+
+            moments.forEach(function (moment) {
+              QUnit.test(['filterInAllBVS：在 All BVS 中 filter 网络时刻', moment, '中匹配', patternURL, '的详情'].join(''), function (assert) {
+                eachAssert(this.network.filterInAllBVS([moment], patternURL, tabId), assert)
+              })
+            })
+          })
+
+          QUnit.module('Derivative', function () {
+            QUnit.module(specialMoment, function () {
+              QUnit.test(['filterInAllBVS', specialMoment, '：在 All BVS 中 filter all 网络时刻中匹配', patternURL, '的详情'].join(''), function (assert) {
+                eachAssert(this.network['filterInAllBVS' + specialMoment](patternURL, tabId), assert)
+              })
+            })
+
+            moments.forEach(function (moment) {
+              QUnit.module(moment, function () {
+                QUnit.test(['filterInAllBVS', moment, '：在 All BVS 中 filter 网络时刻', moment, '中匹配', patternURL, '的详情'].join(''), function (assert) {
+                  eachAssert(this.network['filterInAllBVS' + moment](patternURL, tabId), assert)
+                })
+              })
+            })
+          })
+        })
       })
-    })
 
-    moments.forEach(function (moment) {
-      QUnit.test(["filterInAllBVS", moment, "：在全部的backgroundViewStore中获取网络时刻", moment, "中关于", patternURL, "的请求详情"].join(''), function (assert) {
-        var network = self.create()
+      QUnit.module('Current', function () {
+        QUnit.module('BVS', function () {
+          function eachAssert(result, assert) {
+            Object.keys(result).forEach(function (moment) {
+              var item = result[moment]
 
-        var result = network['filterInAllBVS' + moment](patternURL, tabId)
+              var msg = ['Passed：', '在 Current BVS 中 filter 网络时刻', moment, '匹配', patternURL, '的详情'].join('')
+              assert.notStrictEqual(item, undefined, msg)
 
-        var msg = ['Passed：', '在全部的backgroundViewStore中获取网络时刻', moment, '中关于', patternURL, '的请求详情'].join('')
-        assert.notStrictEqual(result.data, null, msg)
+              var msg = ['Passed：', '检查在 Current BVS 中 filter 网络时刻', moment, '匹配', patternURL, '的详情等于', expected[moment]].join('')
+              assert.deepEqual(item, expected[moment][expected[moment].length - 1], msg)
+            })
+          }
 
-        var msg = ['Passed：', '检查在全部的backgroundViewStore中网络时刻', moment, '中关于', patternURL, '请求详情'].join('')
-        assert.deepEqual(result[moment], expected[moment], msg)
+          QUnit.module('Core', function () {
+            QUnit.test(['filterInCurrentBVS：在 Current BVS 中 filter all 网络时刻中匹配', patternURL, '的详情'].join(''), function (assert) {
+              eachAssert(this.network.filterInCurrentBVS(this.network.moments, patternURL, tabId), assert)
+            })
+
+            moments.forEach(function (moment) {
+              QUnit.test(['filterInCurrentBVS：在 Current BVS 中 filter 网络时刻', moment, '匹配', patternURL, '的详情'].join(''), function (assert) {
+                eachAssert(this.network.filterInCurrentBVS([moment], patternURL, tabId), assert)
+              })
+            })
+          })
+
+          QUnit.module('Derivative', function () {
+            QUnit.module(specialMoment, function () {
+              QUnit.test(['filterInCurrentBVS', specialMoment, '：在 Current BVS 中 filte all 网络时刻中匹配', patternURL, '的详情'].join(''), function (assert) {
+                eachAssert(this.network['filterInCurrentBVS' + specialMoment](patternURL, tabId), assert)
+              })
+            })
+
+            moments.forEach(function (moment) {
+              QUnit.module(moment, function () {
+                QUnit.test(['filterInCurrentBVS', moment, '：在 Current BVS 中 filter 网络时刻', moment, '匹配', patternURL, '的详情'].join(''), function (assert) {
+                  eachAssert(this.network['filterInCurrentBVS' + moment](patternURL, tabId), assert)
+                })
+              })
+            })
+          })
+        })
       })
 
-      QUnit.test(["filterInCurrentBVS", moment, "：在当前的backgroundViewStore中获取网络时刻", moment, "中关于", patternURL, "的请求详情"].join(''), function (assert) {
-        var network = self.create()
+      QUnit.module('Specific', function () {
+        QUnit.module('BVS', function () {
+          function eachAssert(result, assert) {
+            Object.keys(result).forEach(function (moment) {
+              var item = result[moment]
 
-        var result = network['filterInCurrentBVS' + moment](patternURL, tabId)
+              var msg = ['Passed：', '在 All BVS 中 filter 网络时刻', moment, '匹配', patternURL, '的详情'].join('')
+              assert.notStrictEqual(item, undefined, msg)
 
-        var msg = ['Passed：', '在当前的backgroundViewStore中获取网络时刻', moment, '中关于', patternURL, '的请求详情'].join('')
-        assert.notStrictEqual(result.data, null, msg)
+              var msg = ['Passed：', '检查在 All BVS 中 filter 网络时刻', moment, '的详情等于', expected[moment]].join('')
+              assert.deepEqual(item, expected[moment][specificIndex], msg)
+            })
+          }
 
-        var msg = ['Passed：', '检查在当前的backgroundViewStore中网络时刻', moment, '中关于', patternURL, '请求详情'].join('')
-        assert.deepEqual(result[moment], expected[moment][expected[moment].length - 1], msg)
-      })
+          QUnit.module('Core', function () {
+            QUnit.test(['filterInSpecificBVS：在 Specific BVS 中 filter all 网络时刻中匹配', patternURL, '的详情'].join(''), function (assert) {
+              eachAssert(this.network.filterInSpecificBVS(this.network.moments, patternURL, specificIndex, tabId), assert)
+            })
 
-      QUnit.test(["filterInSpecificBVS", moment, "：在指定的backgroundViewStore中获取网络时刻", moment, "中关于", patternURL, "的请求详情"].join(''), function (assert) {
-        var network = self.create()
+            moments.forEach(function (moment) {
+              QUnit.test(['filterInSpecificBVS：在 Specific BVS 中 filter 网络时刻', moment, '匹配', patternURL, '的详情'].join(''), function (assert) {
+                eachAssert(this.network.filterInSpecificBVS([moment], patternURL, specificIndex, tabId), assert)
+              })
+            })
+          })
 
-        var specificIndex = 3
-        var result = network['filterInSpecificBVS' + moment](patternURL, specificIndex, tabId)
+          QUnit.module('Derivative', function () {
+            QUnit.module(specialMoment, function () {
+              QUnit.test(['filterInSpecificBVS', specialMoment, '：在 Specific BVS 中 filter all 网络时刻中匹配', patternURL, '的详情'].join(''), function (assert) {
+                eachAssert(this.network['filterInSpecificBVS' + specialMoment](patternURL, specificIndex, tabId), assert)
+              })
+            })
 
-        var msg = ['Passed：', '在指定的backgroundViewStore中获取网络时刻', moment, '中关于', patternURL, '的请求详情'].join('')
-        assert.notStrictEqual(result.data, null, msg)
-
-        var msg = ['Passed：', '检查在指定的backgroundViewStore中网络时刻', moment, '中关于', patternURL, '请求详情'].join('')
-        assert.deepEqual(result[moment], expected[moment][specificIndex], msg)
+            moments.forEach(function (moment) {
+              QUnit.module(moment, function () {
+                QUnit.test(['filterInSpecificBVS', moment, '：在 Specific BVS 中 filter 网络时刻', moment, '匹配', patternURL, '的详情'].join(''), function (assert) {
+                  eachAssert(this.network['filterInSpecificBVS' + moment](patternURL, specificIndex, tabId), assert)
+                })
+              })
+            })
+          })
+        })
       })
     })
   }
--- server/testPages/remote/main/background/network/units/getAPIs.js
+++ server/testPages/remote/main/background/network/units/getAPIs.js
@@ -1,5 +1,5 @@
 $.extend(Suite.prototype, {
-  testGetAPIs_core: function () {
+  testGetAPIs: function () {
     var self = this
     var tabId = self.config.mockData.tabId
     var viewStores = self.config.mockData.tabStore.viewStores
@@ -15,6 +15,7 @@ $.extend(Suite.prototype, {
       "onCompleted",
       "onErrorOccurred"
     ]
+    var specialMoment = 'onAllMoments'
 
     moments.forEach(function (moment) {
       expected[moment] = []
@@ -26,113 +27,143 @@ $.extend(Suite.prototype, {
       })
     })
 
-    QUnit.test(["getInAllBVS：在全部的backgroundViewStore中获取全部网络时刻的请求详情"].join(''), function (assert) {
-      var network = self.create()
+    var specificIndex = 3
 
-      var result = network.getInAllBVS(network.moments, tabId)
+    QUnit.module('getAPIs', {
+      beforeEach: function () {
+        this.network = self.create()
+      }
+    }, function () {
+      QUnit.module('All', function () {
+        QUnit.module('BVS', function () {
+          function eachAssert(result, assert) {
+            Object.keys(result).forEach(function (moment) {
+              var item = result[moment]
 
-      var msg = ['Passed：', '在全部的backgroundViewStore中获取全部网络时刻的请求详情'].join('')
-      assert.notStrictEqual(result.data, null, msg)
+              var msg = ['Passed：', '在 All BVS 中 get 网络时刻', moment, '的详情'].join('')
+              assert.strictEqual(item.length, 5, msg)
 
-      network.moments.forEach(function (moment) {
-        var msg = ['Passed：', '检查在全部的backgroundViewStore中网络时刻', moment, '的请求详情'].join('')
-        assert.deepEqual(result[moment], expected[moment], msg)
-      })
-    })
+              var msg = ['Passed：', '检查在 All BVS 中 get 网络时刻', moment, '的详情等于', expected[moment]].join('')
+              assert.deepEqual(item, expected[moment], msg)
+            })
+          }
 
-    QUnit.test(["getInCurrentBVS：在当前的backgroundViewStore中获取全部网络时刻的请求详情"].join(''), function (assert) {
-      var network = self.create()
+          QUnit.module('Core', function () {
+            QUnit.test(['getInAllBVS：在 All BVS 中 get all 网络时刻的详情'].join(''), function (assert) {
+              eachAssert(this.network.getInAllBVS(this.network.moments, tabId), assert)
+            })
 
-      var result = network.getInCurrentBVS(network.moments, tabId)
+            moments.forEach(function (moment) {
+              QUnit.test(['getInAllBVS：在 All BVS 中 get 网络时刻', moment, '的详情'].join(''), function (assert) {
+                eachAssert(this.network.getInAllBVS([moment], tabId), assert)
+              })
+            })
+          })
 
-      var msg = ['Passed：', '在当前的backgroundViewStore中获取全部网络时刻的请求详情'].join('')
-      assert.notStrictEqual(result.data, null, msg)
+          QUnit.module('Derivative', function () {
+            QUnit.module(specialMoment, function () {
+              QUnit.test(['getInAllBVS', specialMoment, '：在 All BVS 中 get all 网络时刻的详情'].join(''), function (assert) {
+                eachAssert(this.network['getInAllBVS' + specialMoment](tabId), assert)
+              })
+            })
 
-      network.moments.forEach(function (moment) {
-        var msg = ['Passed：', '检查在当前的backgroundViewStore中网络时刻', moment, '的请求详情'].join('')
-        assert.deepEqual(result[moment], expected[moment][expected[moment].length - 1], msg)
-      })
-    })
-
-    QUnit.test(["getInSpecificBVS：在指定的backgroundViewStore中获取全部网络时刻的请求详情"].join(''), function (assert) {
-      var network = self.create()
-
-      var specificIndex = 3
-      var result = network.getInSpecificBVS(network.moments, specificIndex, tabId)
-
-      var msg = ['Passed：', '在指定的backgroundViewStore[', specificIndex, ']中获取全部网络时刻的请求详情'].join('')
-      assert.notStrictEqual(result.data, null, msg)
-
-      network.moments.forEach(function (moment) {
-        var msg = ['Passed：', '检查在指定的backgroundViewStore[', specificIndex, ']中网络时刻', moment, '的请求详情'].join('')
-        assert.deepEqual(result[moment], expected[moment][specificIndex], msg)
-      })
-    })
-  },
-
-  testGetAPIs_derivative: function () {
-    var self = this
-    var tabId = self.config.mockData.tabId
-    var viewStores = self.config.mockData.tabStore.viewStores
-
-    var expected = {}
-    var moments = [
-      "onBeforeRequest",
-      "onBeforeSendHeaders",
-      "onSendHeaders",
-      "onHeadersReceived",
-      "onBeforeRedirect",
-      "onResponseStarted",
-      "onCompleted",
-      "onErrorOccurred"
-    ]
-
-    moments.forEach(function (moment) {
-      expected[moment] = []
-      viewStores.forEach(function (viewStore) {
-        expected[moment].push({
-          key: moment == 'onBeforeRedirect' || moment == 'onErrorOccurred' ? undefined : moment,
-          value: viewStore[moment]
+            moments.forEach(function (moment) {
+              QUnit.module(moment, function () {
+                QUnit.test(['getInAllBVS', moment, '：在 All BVS 中 get 网络时刻', moment, '的详情'].join(''), function (assert) {
+                  eachAssert(this.network['getInAllBVS' + moment](tabId), assert)
+                })
+              })
+            })
+          })
         })
       })
-    })
 
-    moments.forEach(function (moment) {
-      QUnit.test(["getInAllBVS", moment, "：在全部的backgroundViewStore中获取网络时刻", moment, "的请求详情"].join(''), function (assert) {
-        var network = self.create()
+      QUnit.module('Current', function () {
+        QUnit.module('BVS', function () {
+          function eachAssert(result, assert) {
+            Object.keys(result).forEach(function (moment) {
+              var item = result[moment]
 
-        var result = network['getInAllBVS' + moment](tabId)
+              var msg = ['Passed：', '在 Current BVS 中 get 网络时刻', moment, '的详情'].join('')
+              assert.notStrictEqual(item, undefined, msg)
 
-        var msg = ['Passed：', '在全部的backgroundViewStore中获取网络时刻', moment, '的请求详情'].join('')
-        assert.notStrictEqual(result.data, null, msg)
+              var msg = ['Passed：', '检查在 Current BVS 中 get 网络时刻', moment, '的详情等于', expected[moment][expected[moment].length - 1]].join('')
+              assert.deepEqual(item, expected[moment][expected[moment].length - 1], msg)
+            })
+          }
 
-        var msg = ['Passed：', '检查在全部的backgroundViewStore中网络时刻', moment, '的请求详情'].join('')
-        assert.deepEqual(result[moment], expected[moment], msg)
+          QUnit.module('Core', function () {
+            QUnit.test(['getInCurrentBVS：在 Current BVS 中 get all 网络时刻的详情'].join(''), function (assert) {
+              eachAssert(this.network.getInCurrentBVS(this.network.moments, tabId), assert)
+            })
+
+            moments.forEach(function (moment) {
+              QUnit.test(['getInCurrentBVS：在 Current BVS 中 get 网络时刻', moment, '的详情'].join(''), function (assert) {
+                eachAssert(this.network.getInCurrentBVS([moment], tabId), assert)
+              })
+            })
+          })
+
+          QUnit.module('Derivative', function () {
+            QUnit.module(specialMoment, function () {
+              QUnit.test(['getInCurrentBVS', specialMoment, '：在 Current BVS 中 get all 网络时刻的详情'].join(''), function (assert) {
+                eachAssert(this.network['getInCurrentBVS' + specialMoment](tabId), assert)
+              })
+            })
+
+            moments.forEach(function (moment) {
+              QUnit.module(moment, function () {
+                QUnit.test(['getInCurrentBVS', moment, '：在 Current BVS 中 get 网络时刻', moment, '的详情'].join(''), function (assert) {
+                  eachAssert(this.network['getInCurrentBVS' + moment](tabId), assert)
+                })
+              })
+            })
+          })
+        })
       })
 
-      QUnit.test(["getInCurrentBVS", moment, "：在当前的backgroundViewStore中获取网络时刻", moment, "的请求详情"].join(''), function (assert) {
-        var network = self.create()
+      QUnit.module('Specific', function () {
+        QUnit.module('BVS', function () {
+          function eachAssert(result, assert) {
+            Object.keys(result).forEach(function (moment) {
+              var item = result[moment]
 
-        var result = network['getInCurrentBVS' + moment](tabId)
+              var msg = ['Passed：', '在 Specific BVS[', specificIndex, '] 中 get 网络时刻', moment, '的详情'].join('')
+              assert.notStrictEqual(item, undefined, msg)
 
-        var msg = ['Passed：', '在当前的backgroundViewStore中获取网络时刻', moment, '的请求详情'].join('')
-        assert.notStrictEqual(result.data, null, msg)
+              var msg = ['Passed：', '检查在 Specific BVS[', specificIndex, '] 中 get 网络时刻', moment, '的详情等于', expected[moment][specificIndex]].join('')
+              assert.deepEqual(item, expected[moment][specificIndex], msg)
+            })
+          }
 
-        var msg = ['Passed：', '检查在当前的backgroundViewStore中网络时刻', moment, '的请求详情'].join('')
-        assert.deepEqual(result[moment], expected[moment][expected[moment].length - 1], msg)
-      })
+          QUnit.module('Core', function () {
+            QUnit.test(['getInSpecificBVS：在 Specific BVS 中 get all 网络时刻的详情'].join(''), function (assert) {
+              eachAssert(this.network.getInSpecificBVS(this.network.moments, specificIndex, tabId), assert)
+            })
 
-      QUnit.test(["getInSpecificBVS", moment, "：在指定的backgroundViewStore中检查网络时刻", moment, "的请求详情"].join(''), function (assert) {
-        var network = self.create()
+            moments.forEach(function (moment) {
+              QUnit.test(['getInSpecificBVS：在 Specific BVS 中 get 网络时刻', moment, '的详情'].join(''), function (assert) {
+                eachAssert(this.network.getInSpecificBVS([moment], specificIndex, tabId), assert)
+              })
+            })
+          })
 
-        var specificIndex = 3
-        var result = network['getInSpecificBVS' + moment](specificIndex, tabId)
+          QUnit.module('Derivative', function () {
+            QUnit.module(specialMoment, function () {
+              QUnit.test(['getInSpecificBVS', specialMoment, '：在 Specific BVS 中 get all 网络时刻的详情'].join(''), function (assert) {
+                eachAssert(this.network['getInSpecificBVS' + specialMoment](specificIndex, tabId), assert)
+              })
+            })
 
-        var msg = ['Passed：', '在指定的backgroundViewStore[', specificIndex, ']中获取网络时刻', moment, '的请求详情'].join('')
-        assert.notStrictEqual(result.data, null, msg)
-
-        var msg = ['Passed：', '检查在指定的backgroundViewStore[', specificIndex, ']中网络时刻', moment, '的请求详情'].join('')
-        assert.deepEqual(result[moment], expected[moment][specificIndex], msg)
+            moments.forEach(function (moment) {
+              QUnit.module(moment, function () {
+                QUnit.test(['getInSpecificBVS', moment, '：在 Specific BVS 中 get 网络时刻', moment, '的详情'].join(''), function (assert) {
+                  eachAssert(this.network['getInSpecificBVS' + moment](specificIndex, tabId), assert)
+                })
+              })
+            })
+          })
+        })
       })
     })
   }
