bea5e29b2c9302d13c30dac87c685f6e8c2b7252
shangfeiSF <tony20100125@126.com>
2016-07-25 18:02:06 +0800

    chross 2 项目开发：injection 消息格式更新，监听回调位置统一到port（重要升级）

---
+++ public/remote/src/main/injection/web_modules/network.js
@@ -0,0 +1,241 @@
+var message = require('message')
+
+function Network(chross, config) {
+  var config = config || {}
+
+  var defaultConfig = {}
+
+  this.config = $.extend(true, defaultConfig, config)
+
+  this.listenerInfosMap = {}
+
+  this.chross = chross
+
+  this.init()
+}
+
+$.extend(Network.prototype,
+  {
+    existsInAllViews: function (moments) {
+      var self = this
+      var port = self.chross.port
+
+      var listener = $.Deferred()
+
+      self.register(listener)
+        .then(function (sign) {
+          port.post({
+            sign: sign,
+            command: 'existsAVN',
+            params: {
+              moments: moments
+            }
+          })
+        })
+
+      return listener.promise()
+    },
+
+    existsInCurrentView: function (moments) {
+      var self = this
+      var port = self.chross.port
+
+      var listener = $.Deferred()
+
+      self.register(listener)
+        .then(function (sign) {
+          port.post({
+            sign: sign,
+            command: 'existsCVN',
+            params: {
+              moments: moments
+            }
+          })
+        })
+
+      return listener.promise()
+    },
+
+    existsInSpecificView: function (moments, index) {
+      var self = this
+      var port = self.chross.port
+
+      var listener = $.Deferred()
+
+      self.register(listener)
+        .then(function (sign) {
+          port.post({
+            sign: sign,
+            command: 'existsSVN',
+            params: {
+              moments: moments,
+              index: index
+            }
+          })
+        })
+
+      return listener.promise()
+    }
+  },
+  {
+    getInAllViews: function (moments) {
+      var self = this
+      var port = self.chross.port
+
+      var listener = $.Deferred()
+
+      self.register(listener)
+        .then(function (sign) {
+          port.post({
+            sign: sign,
+            command: 'getAVN',
+            params: {
+              moments: moments
+            }
+          })
+        })
+
+      return listener.promise()
+    },
+
+    getInCurrentView: function (moments) {
+      var self = this
+      var port = self.chross.port
+
+      var listener = $.Deferred()
+
+      self.register(listener)
+        .then(function (sign) {
+          port.post({
+            sign: sign,
+            command: 'getCVN',
+            params: {
+              moments: moments
+            }
+          })
+        })
+
+      return listener.promise()
+    },
+
+    getInSpecificView: function (moments, index) {
+      var self = this
+      var port = self.chross.port
+
+      var listener = $.Deferred()
+
+      self.register(listener)
+        .then(function (sign) {
+          port.post({
+            sign: sign,
+            command: 'getSVN',
+            params: {
+              moments: moments,
+              index: index
+            }
+          })
+        })
+
+      return listener.promise()
+    }
+  },
+  {
+    filterInAllViews: function (moments, pattern) {
+      var self = this
+      var port = self.chross.port
+
+      var listener = $.Deferred()
+
+      self.register(listener)
+        .then(function (sign) {
+          port.post({
+            sign: sign,
+            command: 'filterAVN',
+            params: {
+              moments: moments,
+              pattern: pattern
+            }
+          })
+        })
+
+      return listener.promise()
+    },
+
+    filterInCurrentView: function (moments, pattern) {
+      var self = this
+      var port = self.chross.port
+
+      var listener = $.Deferred()
+
+      self.register(listener)
+        .then(function (sign) {
+          port.post({
+            sign: sign,
+            command: 'filterCVN',
+            params: {
+              moments: moments,
+              pattern: pattern
+            }
+          })
+        })
+
+      return listener.promise()
+    },
+
+    filterInSpecificView: function (moments, pattern, index) {
+      var self = this
+      var port = self.chross.port
+
+      var listener = $.Deferred()
+
+      self.register(listener)
+        .then(function (sign) {
+          port.post({
+            sign: sign,
+            command: 'filterSVN',
+            params: {
+              moments: moments,
+              pattern: pattern,
+              index: index
+            }
+          })
+        })
+
+      return listener.promise()
+    }
+  },
+  {
+    resolve: function (msg) {
+      var self = this
+      var uuid = msg.sign.uuid
+
+      if (self.listenerInfosMap.hasOwnProperty(uuid)) {
+        self.listenerInfosMap[uuid].resolve(msg)
+      } else {
+        console.error('Can not resolve!')
+      }
+    },
+
+    register: function (listener) {
+      var self = this
+      var uuid = message.generateUuid()
+
+      var defer = $.Deferred()
+      var promise = defer.promise()
+
+      self.listenerInfosMap[uuid] = listener
+
+      defer.resolve({
+        module: self.constructor.name,
+        uuid: uuid
+      })
+
+      return promise
+    },
+
+    init: function () {
+    }
+  }
+)
+
+module.exports = Network
